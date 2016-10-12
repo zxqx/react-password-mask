9063dea311197cc33131a2ba706f1ced5a383715
Zak Angelle <zaknotzak@gmail.com>
2016-10-12 15:32:10 -0500

    Clean up tests

--- test/index.js
+++ test/index.js
@@ -13,78 +13,96 @@ let password = '';
 function onShow() {}
 
 describe('<PasswordMask />', () => {
-  before(() => {
-    wrapper = shallow(
-      <PasswordMask
-        id="password"
-        name="password"
-        className="form-field"
-        placeholder="Enter password"
-        value={password}
-        onChange={e => password = e.target.value}
-        inputStyles={{ borderColor: 'aqua' }}
-        buttonStyles={{ background: 'smoke' }}
-        onShow={onShow}
-      />
-    );
-  });
-
   it('renders password field', () => {
+    const wrapper = shallow(
+      <PasswordMask value={password} />
+    );
+
     expect(wrapper.find('input[type="password"]')).to.have.length(1);
   });
 
   it('renders text field', () => {
+    const wrapper = shallow(
+      <PasswordMask value={password} />
+    );
+
     expect(wrapper.find('input[type="text"]')).to.have.length(1);
   });
 
   it('renders show/hide button', () => {
+    const wrapper = shallow(
+      <PasswordMask value={password} />
+    );
+
     expect(wrapper.find('a')).to.have.length(1);
   });
 
   it('sets value passed from props', () => {
+    const wrapper = shallow(
+      <PasswordMask value={password} />
+    );
+
     expect(wrapper.instance().props.value).to.equal('');
   });
 
   it('sets id passed from props', () => {
+    const wrapper = shallow(
+      <PasswordMask
+        id="password"
+        value={password}
+      />
+    );
+
     expect(wrapper.instance().props.id).to.equal('password');
   });
 
   it('sets name passed from props', () => {
+    const wrapper = shallow(
+      <PasswordMask
+        name="password"
+        value={password}
+      />
+    );
+
     expect(wrapper.instance().props.name).to.equal('password');
   });
 
   it('sets className passed from props', () => {
+    const wrapper = shallow(
+      <PasswordMask
+        className="form-field"
+        value={password}
+      />
+    );
+
     expect(wrapper.instance().props.className).to.equal('form-field');
   });
 
   it('sets placeholder passed from props', () => {
+    const wrapper = shallow(
+      <PasswordMask
+        placeholder="Enter password"
+        value={password}
+      />
+    );
+
     expect(wrapper.instance().props.placeholder).to.equal('Enter password');
   });
 
   it('sets onChange callback passed from props', () => {
-    expect(wrapper.instance().props.onChange).to.not.be.undefined;
-  });
-
-  it('applies input styles passed from props', () => {
-    const input = wrapper.find('input[type="password"]');
-    expect(input).to.have.style('border-color', 'aqua');
-  });
-
-  it('applies button styles passed from props', () => {
-    const showHideButton = wrapper.find('a');
-    expect(showHideButton).to.have.style('background', 'smoke');
-  });
-
-  it('updates internal showPassword state', () => {
-    let wrapper = shallow(
+    const wrapper = shallow(
       <PasswordMask
-        id="password"
-        name="password"
-        className="form-field"
-        placeholder="Enter password"
         value={password}
         onChange={e => password = e.target.value}
       />
+    );
+
+    expect(wrapper.instance().props.onChange).to.not.be.undefined;
+  });
+
+  it('updates internal showPassword state', () => {
+    const wrapper = shallow(
+      <PasswordMask value={password} />
     );
 
     const showHideButton = wrapper.find('a');
@@ -93,15 +111,8 @@ describe('<PasswordMask />', () => {
   });
 
   it('updates internal hasBeenFocused state', () => {
-    let wrapper = shallow(
-      <PasswordMask
-        id="password"
-        name="password"
-        className="form-field"
-        placeholder="Enter password"
-        value={password}
-        onChange={e => password = e.target.value}
-      />
+    const wrapper = shallow(
+      <PasswordMask value={password} />
     );
 
     const input = wrapper.find('input[type="password"]');
@@ -109,7 +120,38 @@ describe('<PasswordMask />', () => {
     expect(wrapper.instance().state.hasBeenFocused).to.equal(true);
   });
 
+  it('applies input styles passed from props', () => {
+    const wrapper = shallow(
+      <PasswordMask
+        value={password}
+        inputStyles={{ borderColor: 'aqua' }}
+      />
+    );
+
+    const input = wrapper.find('input[type="password"]');
+    expect(input).to.have.style('border-color', 'aqua');
+  });
+
+  it('applies button styles passed from props', () => {
+    const wrapper = shallow(
+      <PasswordMask
+        value={password}
+        buttonStyles={{ background: 'smoke' }}
+      />
+    );
+
+    const showHideButton = wrapper.find('a');
+    expect(showHideButton).to.have.style('background', 'smoke');
+  });
+
   it('calls onChange callback', () => {
+    const wrapper = shallow(
+      <PasswordMask
+        value={password}
+        onChange={e => password = e.target.value}
+      />
+    );
+
     const input = wrapper.find('input[type="password"]');
     input.simulate('change', { target: { value: 'bval' } });
 
@@ -119,14 +161,9 @@ describe('<PasswordMask />', () => {
   it('calls onShow callback', () => {
     const onShow = sinon.spy();
 
-    let wrapper = shallow(
+    const wrapper = shallow(
       <PasswordMask
-        id="password"
-        name="password"
-        className="form-field"
-        placeholder="Enter password"
         value={password}
-        onChange={e => password = e.target.value}
         onShow={onShow}
       />
     );
@@ -139,14 +176,9 @@ describe('<PasswordMask />', () => {
   it('calls onHide callback', () => {
     const onHide = sinon.spy();
 
-    let wrapper = shallow(
+    const wrapper = shallow(
       <PasswordMask
-        id="password"
-        name="password"
-        className="form-field"
-        placeholder="Enter password"
         value={password}
-        onChange={e => password = e.target.value}
         onHide={onHide}
       />
     );
@@ -160,14 +192,9 @@ describe('<PasswordMask />', () => {
   it('calls onToggle callback', () => {
     const onToggle = sinon.spy();
 
-    let wrapper = shallow(
+    const wrapper = shallow(
       <PasswordMask
-        id="password"
-        name="password"
-        className="form-field"
-        placeholder="Enter password"
         value={password}
-        onChange={e => password = e.target.value}
         onToggle={onToggle}
       />
     );
@@ -181,15 +208,8 @@ describe('<PasswordMask />', () => {
   it('cancels mousedown event', () => {
     const preventDefault = sinon.spy();
 
-    let wrapper = shallow(
-      <PasswordMask
-        id="password"
-        name="password"
-        className="form-field"
-        placeholder="Enter password"
-        value={password}
-        onChange={e => password = e.target.value}
-      />
+    const wrapper = shallow(
+      <PasswordMask value={password} />
     );
 
     const showHideButton = wrapper.find('a');
