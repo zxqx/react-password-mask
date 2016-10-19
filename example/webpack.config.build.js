659693e1f3d648b62c6c52ab95845dcbfee8e384
Zak Angelle <zaknotzak@gmail.com>
2016-10-19 12:43:37 -0500

    Set NODE_ENV env variable

--- example/webpack.config.build.js
+++ example/webpack.config.build.js
@@ -19,6 +19,11 @@ module.exports = {
       filename: 'index.[hash].css',
       allChunks: true
     }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true,
