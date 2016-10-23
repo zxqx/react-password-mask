45c6a34014d6bd08da4da6f8193e2b6a3c8d67de
Zak Angelle <zaknotzak@gmail.com>
2016-10-23 02:26:30 -0500

    Use path.join

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
+const path = require('path');
 const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 const libraryName = 'passwordMask';
 
 module.exports = {
-  entry: __dirname + '/src/index.js',
+  entry: path.join(__dirname, 'src/index.js'),
   output: {
-    path: __dirname + '/lib',
+    path: path.join(__dirname, 'lib'),
     filename: libraryName + '.min.js',
     library: libraryName,
     libraryTarget: 'umd',
