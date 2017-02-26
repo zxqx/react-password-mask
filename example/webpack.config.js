dd5da3c1d4be3e6aa78b72f8d3b1ac67c6ce0667
Zak Angelle <zaknotzak@gmail.com>
2017-02-25 20:57:04 -0600

    Update webpack production build regex

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /(\.jsx|\.js)$/,
+        test: /\.js/,
         use: 'babel-loader',
         include: [
           path.resolve(__dirname),
