4ec5ff099aa547a6388095c80379c501b6b0d0be
Zak Angelle <zaknotzak@gmail.com>
2017-02-25 20:45:00 -0600

    Update webpack build regex

--- example/webpack.config.build.js
+++ example/webpack.config.build.js
@@ -33,7 +33,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /(\.jsx|\.js)$/,
+        test: /\.js/,
         use: 'babel-loader',
         include: [
           path.resolve(__dirname),
