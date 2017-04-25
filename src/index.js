fea7de940864917899744d39a32afe8ae26ed8b6
Zak Angelle <zaknotzak@gmail.com>
2017-04-25 15:41:17 -0500

    Use standalone prop-types package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { inputStyles, buttonStyles } from './styles.js';
 
 export default class PasswordMask extends Component {
