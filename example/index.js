fe8710cdb41ae048c9d3af0152a12a5bb8576fbd
Zak Angelle <zaknotzak@gmail.com>
2017-09-22 19:01:22 -0500

    Add github pages example

--- example/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Example from './Example';
-import './style.css';
-
-render(
-  <Example />,
-  document.getElementById('root')
-);
